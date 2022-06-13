6befac9daf326dacdfdd642541dabcfb822d8391
Alexander-Rus <alex.rus10@gmail.com>
2022-06-13 16:25:12 -0400

    Commit version Mon Jun 13 04:25:12 PM EDT 2022

--- 01_two_sum/java_solution/attempt1.java
+++ 01_two_sum/java_solution/attempt1.java
@@ -1,7 +1,11 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
 class Solution{
-	public int[] twoSum(int[] numbers, int target) {
+	//we are returning an array of integers, we are taking in an array called numbers, and an integer target
+	public static int[] twoSum(int[] numbers, int target) {
 	    int[] result = new int[2];
-	    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 	    for (int i = 0; i < numbers.length; i++) {
 		if (map.containsKey(target - numbers[i])) {
 		    result[1] = i;
@@ -11,6 +15,16 @@ class Solution{
 		map.put(numbers[i], i);
 	    }
 	    return result;
+	}
+	
+	public static int multiplyByTwo(int number) {
+		return number * 2;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = new int[]{1,2,3,4,5};
+		int[] result = twoSum(arr, 9);
+		System.out.println(Arrays.toString(result));
 	}
 }
 
